0e57030b5cef49812c9d210c6c5ea1473f46fec6
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2023-07-24 22:19:37 +0300

    done:integration

--- lib/core/navigation/app_router.dart
+++ lib/core/navigation/app_router.dart
@@ -1,19 +1,16 @@
 import 'package:go_router/go_router.dart';
 
 import '../../features/admin/presentation/admin_screen.dart';
-import '../../features/admin_approb/presentation/admin_approb_screen.dart';
 import '../../features/auth/presentation/auth_screen.dart';
 import '../../features/home/presentation/home_screen.dart';
 import '../../features/new_request/presentation/new_request_screen.dart';
 import '../../features/subscription/presentation/subscription_screen.dart';
-import '../../features/vacations_list/presentation/vacation_search_screen.dart';
-import '../../features/vacations_list/presentation/vacations_list_screen.dart';
 import '../../features/welcome/welcome_screen.dart';
 import '../utils/constants/route_name.dart';
 
 class AppRouter {
   static GoRouter configurations = GoRouter(
-    initialLocation: RouteName.admin,
+    initialLocation: RouteName.welcome,
     routes: [
       GoRoute(
         path: RouteName.welcome,
@@ -38,18 +35,6 @@ class AppRouter {
       GoRoute(
         path: RouteName.request,
         builder: (context, state) => const NewRequestScreen(),
-      ),
-      GoRoute(
-        path: RouteName.adminApprob,
-        builder: (context, state) => const AdminApprobScreen(),
-      ),
-      GoRoute(
-        path: RouteName.vacations,
-        builder: (context, state) => const VacationsListScreen(),
-      ),
-      GoRoute(
-        path: RouteName.search,
-        builder: (context, state) => const VacationSearchScreen(),
       ),
       GoRoute(
         path: RouteName.admin,
