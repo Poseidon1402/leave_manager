6ac8bf3735293f2cd911abce5b65adbe23faa984
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2023-07-25 11:25:35 +0300

    add:internet access permission and firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app_theme.dart';
 import 'core/navigation/app_router.dart';
-
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
   );
+  await Firebase.initializeApp();
   runApp(const LeaveManager());
 }
 
